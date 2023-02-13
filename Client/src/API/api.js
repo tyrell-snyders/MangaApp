0fb3033ad61ea3d186bfa6f2da5c2bd76791d7af
Tyrell Snyders <68440041+SFSADespair@users.noreply.github.com>
2023-02-13 18:00:40 +0200

    Update api.js

--- Client/src/API/api.js
+++ Client/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://192.168.18.94:3000//',
+    baseURL: 'http://localhost:3000//',
     headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         "Access-Control-Allow-Origin": "*",
